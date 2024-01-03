fd7b23d2adbe468aed11c74aa3b6cf9336f9b8b5
Anantha-K <89961607+Anantha-K@users.noreply.github.com>
2024-01-03 22:28:56 +0530

    bleh

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
   const [buttonPressed, setButtonPressed] = useState(false);
 
   const generateQRCode = () => {
-    const combinedValue = `${input1}+${input2}`;
+    const combinedValue = `${input1}-${input2}`;
     return combinedValue;
   };
 
   const handleGenerateQR = () => {
     if(input1!=="" || input2!==""){
 
-      setButtonPressed(true);
+      setButtonPressed(false);
     }
   };
 
@@ -77,7 +77,7 @@ Generate QR
 
 
       </div>
-      <button  onClick={handleGenerateQR} href="#_" class="inline-flex mt-3 items-center justify-center h-16 px-10 py-0 text-xl font-semibold text-center text-gray-200 no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-600 border-solid rounded-full cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline">
+      <button href="#_" class="inline-flex mt-3 items-center justify-center h-16 px-10 py-0 text-xl font-semibold text-center text-gray-200 no-underline align-middle transition-all duration-300 ease-in-out bg-transparent border-2 border-gray-600 border-solid rounded-full cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline">
 Send Mail
 </button>
 
